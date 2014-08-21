44770d2e8a22ea3f24733c8d306df492fb554d92
Aaron Gonzales <xysmas@gmail.com>
2014-08-20 22:51:57 -0600

    added AStar.java

---
+++ src/astar/AStar.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package astar;
+
+/**
+ * @author agonzales
+ * Implements a version of the A* algorithm using
+ * a priority queue. 
+ * Will change as we get program specs on how the graph is 
+ * represented. 
+ */
+public class AStar {
+	
+}
