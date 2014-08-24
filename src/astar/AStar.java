5155921446388f686c4d8746f355287c837c21f7
Aaron Gonzales <xysmas@gmail.com>
2014-08-24 10:54:30 -0600

    put data from Charlie's lab work into the proper places for the project, no mods yet other that refactoring Map

--- src/astar/AStar.java
+++ src/astar/AStar.java
@@ -3,13 +3,131 @@
  */
 package astar;
 
+import MapData;
+import SortMapData;
+import constructPath;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
- * @author agonzales
- * Implements a version of the A* algorithm using
- * a priority queue. 
- * Will change as we get program specs on how the graph is 
- * represented. 
+ * @author agonzales Implements a version of the A* algorithm using a priority
+ *         queue. Will change as we get program specs on how the graph is
+ *         represented.
  */
 public class AStar {
-	
+
+	/**
+	 * Implements the Astar best first search algorithm to find the shortest
+	 * path from the start to the goal.
+	 * 
+	 * @return A string containing the cost and the list of moves to perform to
+	 *         reach the goal as cheaply as possible.
+	 */
+	public String shortestPath() {
+		Map start = new Map(startx, starty, map[starty][startx]);
+		Map visit;
+		Comparator<Map> compare = new SortMapData();
+		PriorityQueue<Map> pq = new PriorityQueue<Map>(25, compare);
+		int visitx;
+		int visity;
+		int westx;
+		int eastx;
+		int southy;
+		int northy;
+		pq.add(start);
+		ArrayList<Map> visited = new ArrayList<Map>();
+		while ((visit = pq.poll()) != null) {
+			visited.add(visit);
+			visitx = visit.getX();
+			visity = visit.getY();
+			if (visitx == finishx && visity == finishy) {
+				return constructPath(visit);
+			}
+			westx = visitx - 1;
+			eastx = visitx + 1;
+			southy = visity + 1;
+			northy = visity - 1;
+			if (westx >= 0) {
+				Map westNeighbor = new Map(westx, visity, visit.getPathCost()
+						+ map[visity][westx], visit);
+				if (!visited.contains(westNeighbor)) {
+					pq.add(westNeighbor);
+				}
+			}
+			if (eastx < map[0].length) {
+				Map eastNeighbor = new Map(eastx, visity, visit.getPathCost()
+						+ map[visity][eastx], visit);
+				if (!visited.contains(eastNeighbor)) {
+					pq.add(eastNeighbor);
+				}
+			}
+			if (southy < map.length) {
+				Map southNeighbor = new Map(visitx, southy, visit.getPathCost()
+						+ map[southy][visitx], visit);
+				if (!visited.contains(southNeighbor)) {
+					pq.add(southNeighbor);
+				}
+			}
+			if (northy >= 0) {
+				Map northNeighbor = new Map(visitx, northy, visit.getPathCost()
+						+ map[northy][visitx], visit);
+				if (!visited.contains(northNeighbor)) {
+					pq.add(northNeighbor);
+				}
+			}
+		}
+		return "Failure.";
+	}
+
+	/**
+	 * A method that uses each MapData's origin field to construct the path that
+	 * was taken from the finish back to the start.
+	 * 
+	 * @return The list of moves to reach the finish from the start.
+	 */
+	public String constructPath(Map finish) {
+		Map previous = finish;
+		Map current = finish.getOrigin();
+		String path = "";
+		while (current != null) {
+			/*
+			 * This code remains as a snippet to indicate what was done to
+			 * create the generated image. for(int i = 0; i < 16; i++) { for(int
+			 * j = 0; j < 16; j++) { pic.setRGB(16*previous.getX() + i,
+			 * 16*previous.getY() + j, 255, 0, 0); } }
+			 */
+			path += pathHelper(current, previous);
+			previous = current;
+			current = current.getOrigin();
+		}
+		// pic.saveImage();
+		return finish.getFullCost() + "\n"
+				+ (new StringBuffer(path).reverse().toString());
+	}
+
+	/**
+	 * A helper for constructPath that compares the x and y values of two nodes
+	 * to determine whether a move of left, right, up, or down was made.
+	 * 
+	 * @return A single character string of L, R, D, or U.
+	 * @see constructPath
+	 */
+	public String pathHelper(Map a, Map b) {
+		if ((a.getX() - b.getX()) == 1) {
+			return "L";
+		}
+		if ((b.getX() - a.getX()) == 1) {
+			return "R";
+		}
+		if ((a.getY() - b.getY()) == 1) {
+			return "U";
+		}
+		if ((b.getY() - a.getY()) == 1) {
+			return "D";
+		}
+		return "X";
+	}
+
 }
--- src/astar/ReadMap.java
+++ src/astar/ReadMap.java
@@ -4,94 +4,65 @@
 package astar;
 
 import java.io.*;
-
-
-/**
- * @author agonzales
- *
- */
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ReadMap {
-  public static int[][] map;
-  public static int startx;
-  public static int starty;
-  public static int finishx;
-  public static int finishy;
 
-  public ReadMap() {
-  }
+	public static int[][] map = new int[30][28];
+	public static int startx;
+	public static int starty;
+	public static int finishx;
+	public static int finishy;
 
-  public void readMap() {
-    map = new int[30][28];
-    File file = new File("/Users/cclauss/Documents/workspace/cs351lab1/MapWeights");
-    FileReader filereader;
-    try {
-      filereader = new FileReader(file);
-    } catch (FileNotFoundException e) {
-      System.out.println("Error opening file MapWeights");
-      return;
-    }
-    BufferedReader reader = new BufferedReader(filereader);
-    String line;
-      try {
-        int row = 0;
-        int column = 0;
-        int temp = 0;
-        while((line = reader.readLine()) != null) {
-          for(char c : line.toCharArray()) {
-            switch(c) {
-              case 'X':
-                temp = 100;
-                break;
-              case '0':
-                temp = 1;
-                break;
-              case '1':
-                temp = 2;
-                break;
-              case '2':
-                temp = 4;
-                break;
-              case '>':
-                temp = 0;
-                startx = row;
-                starty = column;
-                break;
-              case '!':
-                temp = 0;
-                finishx = row;
-                finishy = column;
-                break;
-              default:
-                temp = -1;
-                break;
-            }
+	ReadMap() {
+	};
 
-            map[row][column] = temp;
-            column++;
-          }
-          column = 0;
-          row++;
-        }
-      } catch (IOException e) {
-        System.out.println("Error during parsing.");
-      }
-  }
-
-
-  public static void main(String[] args) {
-    ReadMap star = new ReadMap();
-    star.readMap();
-    for(int i = 0; i < 30; i++) {
-      for(int j = 0; j < 28; j++) {
-        
-      }
-    }
-  }
-
+	public void readMap() throws MalformedURLException, IOException {
+		// I made this a URL mostly as practice, but also so that there wouldn't
+		// be
+		// file system issues when I turned in my code.
+		URL mapfile = new URL("https://www.cs.unm.edu/~toriadam/mapweight.txt");
+		Reader fileReader = new InputStreamReader(mapfile.openStream());
+		BufferedReader reader = new BufferedReader(fileReader);
+		String line;
+		int row = 0;
+		int column = 0;
+		int temp = 0;
+		while ((line = reader.readLine()) != null) {
+			for (char c : line.toCharArray()) {
+				switch (c) {
+				case 'X':
+					temp = 100;
+					break;
+				case '0':
+					temp = 1;
+					break;
+				case '1':
+					temp = 2;
+					break;
+				case '2':
+					temp = 4;
+					break;
+				case '>':
+					temp = 0;
+					startx = column;
+					starty = row;
+					break;
+				case '!':
+					temp = 0;
+					finishx = column;
+					finishy = row;
+					break;
+				default:
+					temp = -1;
+					break;
+				}
+				map[row][column] = temp;
+				column++;
+			}
+			column = 0;
+			row++;
+		}
+	}
 }
