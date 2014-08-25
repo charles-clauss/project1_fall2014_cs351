053e0e7685e6bb6e65cbb689488a6b33df8f429d
Charles Clauss <charles-clauss@users.noreply.github.com>
2014-08-25 08:44:02 -0600

    Update AntController.java

--- src/controller/AntController.java
+++ src/controller/AntController.java
@@ -8,13 +8,9 @@ public class AntController {
   AntFactory af = new AntFactory();
   CommData cd = new CommData(NestNameEnum.WOOD, TeamNameEnum.Antithesis);
 
-  for(int i = 0; i < cd.myAntList.size(); i++) {
-    //exec.submit(new af.makeAnt(cd.myAntList.get(i)));
-  }
-  /*
   for(AntData data : cd.myAntList) {
-    exec.submit(new af.makeAnt(data.antType));
+    exec.submit(new af.makeAnt(data));
   }
   exec.shutdown();
-  while(!exec.isTerminated()) {} */
+  while(!exec.isTerminated()) {}
 }
