e7a776b94cad92523a5752a6fa28b43e15f2e7f2
Aaron Gonzales <xysmas@gmail.com>
2014-08-29 10:21:14 -0600

    updated code with new standards. hard set in eclipse so i don't have to do it again

--- src/gamesate/PictureMod.java
+++ src/gamesate/PictureMod.java
@@ -3,59 +3,64 @@ package gamesate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PictureMod {
+public class PictureMod
+{
 
-	PictureMod (){
-	
-	}
-	/**
-	 * 
-	 * @param pic
-	 * @param x
-	 * @param y
-	 * @return
-	 */
-	public List getPixelVal(Picture pic, int x, int y){
-		int red = pic.getRed(x, y);
-		int green = pic.getGreen(x,y);
-		int blue = pic.getBlue(x, y);
-		List pixels = new List();
-		pixels.add(red,green,blue);
-		
-		return pixels;
-	}
-	
-	/**
-	 * will iterate over a range in the picture to get values
-	 * @param start
-	 * @param stop
-	 * @return
-	 */
-	public ArrayList iteratePixels(int startx, int stopx, int starty, int stopy){
-		ArrayList al = new ArrayList(); 
-				
-		return al;
-	}
-	
-	
-	public static void main(String[] args) {
-		System.out.println("testing pic class");
-		
-		Picture pic = new Picture();
-		
-		//test blue values
-		int red = pic.getRed(0, 0);
-		int blue = pic.getBlue(0, 0);
-		int green = pic.getGreen(0,0);
-		
-		System.out.println("blue = " + blue);
-		System.out.println("red = " + red);
-		System.out.println("green = " + green);
-		
-		System.out.println(getPixelVal(pic, 10,10));
-		
-		
-		
-	}
+  PictureMod()
+  {
+
+  }
+
+  /**
+   * @param pic
+   * @param x
+   * @param y
+   * @return
+   */
+  public List<Integer> getPixelVal(Picture pic, int x, int y)
+  {
+    int red = pic.getRed(x, y);
+    int green = pic.getGreen(x, y);
+    int blue = pic.getBlue(x, y);
+    List<Integer> pixels = new ArrayList<Integer>();
+    pixels.add(red);
+    pixels.add(green);
+    pixels.add(blue);
+
+    return pixels;
+  }
+
+  /**
+   * will iterate over a range in the picture to get values
+   * 
+   * @param start
+   * @param stop
+   * @return
+   */
+  public List<Integer> iteratePixels(int startx, int stopx, int starty,
+      int stopy)
+  {
+    List<Integer> al = new ArrayList<Integer>();
+    return al;
+  }
+
+  public static void main(String[] args)
+  {
+    System.out.println("testing pic class");
+
+    Picture pic = new Picture();
+
+    // test blue values
+    int red = pic.getRed(0, 0);
+    int blue = pic.getBlue(0, 0);
+    int green = pic.getGreen(0, 0);
+
+    System.out.println("blue = " + blue);
+    System.out.println("red = " + red);
+    System.out.println("green = " + green);
+
+    // System.out.println(getPixelVal(pic, 10, 10));
+
+  }
 
 }
