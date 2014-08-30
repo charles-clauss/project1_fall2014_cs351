11f7ee7dfd224e9cda2c28dcc2c825b14defc9e7
Aaron Gonzales <agonzales@cs.unm.edu>
2014-08-29 23:53:04 -0600

    added JUnit test class

---
+++ src/gamesate/GraphTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package gamesate;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author agonzales
+ *
+ */
+public class GraphTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
