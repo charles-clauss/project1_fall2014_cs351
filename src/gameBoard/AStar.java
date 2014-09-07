a73bc3c3aca89c3ddfeb4a22a2dfc388cca0eb55
Aaron Gonzales <xysmas@gmail.com>
2014-09-07 16:56:24 -0600

    Basic pathfinding workss

--- src/gameBoard/AStar.java
+++ src/gameBoard/AStar.java
@@ -1,9 +1,4 @@
-/**
- * 
- */
-package astar;
-
-import gameBoard.*;
+package gameBoard;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -45,53 +40,101 @@ public class AStar extends Observer
 
   public List<Coordinate> findPath(Coordinate start, Coordinate goal)
   {
-    Coordinate current;
+    Coordinate current = start;
     Comparator<Coordinate> compare = new SortCoordinate();
     List<Coordinate> nullList = new ArrayList<Coordinate>();
     List<Coordinate> closedList = new ArrayList<Coordinate>();
     List<Coordinate> cameFromList = new ArrayList<Coordinate>();
+    // List<Coordinate> finishedPath = new ArrayList<Coordinate>();
     Queue<Coordinate> openList = new PriorityQueue<Coordinate>(25, compare);
-
-    int gScore = 0;
-    int fScore = gScore;
-    
     nullList = null;
-    openList.add(start);
-    System.out.println("Astar: pq peek = " + openList.peek());
-    
-    cameFromList.add(start);
+    openList.add(current);
+//    System.out.println("Astar: pq peek = " + openList.peek());
+    current.setDistanceToGoal(manDistance(current, goal));
 
     while (openList.peek() != null)
     {
       current = openList.poll();
-      System.out.println("Astar: current = " + current + "X,Y :"
-          + " " + current.getX() + " " + current.getY());
       closedList.add(current);
+      cameFromList.add(current);
 
       if (current.areEqual(goal) == true)
       {
+        // System.out.println(constructPath(goal));
         return cameFromList;
       }
 
-      for (Coordinate neighbor : current.getNeighbors())
-      {
-        // System.out.println("Neighbors X,Y: " + neighbor.getX() + "," +
-        // neighbor.getY() +", weight = " + neighbor.getWeight() );
-        if (closedList.contains(neighbor))
-        {
-          continue;
-        }
-        
-        int tempDistance = gScore + estimateDistance(neighbor, goal, gScore);
-        
-        if (openList.contains(neighbor)== false && tempDistance < )
+      List<Coordinate> neighbors = current.getNeighbors();
 
+      for (Coordinate n : neighbors)
+      {
+        n.setDistanceToGoal(n.getWeight() + manDistance(n, goal));
+        if (!closedList.contains(n))
+        {
+          openList.add(n);
+        }
       } // end for
+      System.out.println(openList.peek().getDistanceToGoal());
 
     } // end while
     // failure
     return nullList;
   } // end findpath
 
-  
+  /**
+   * A method that uses each MapData's origin field to construct the path that
+   * was taken from the finish back to the start.
+   * 
+   * @return The list of moves to reach the finish from the start.
+   */
+  public String constructPath(Coordinate finish)
+  {
+    Coordinate previous = finish;
+    Coordinate current = finish.getParent();
+    String path = "";
+    while (current != null)
+    {
+      /*
+       * This code remains as a snippet to indicate what was done to create the
+       * generated image. for(int i = 0; i < 16; i++) { for(int j = 0; j < 16;
+       * j++) { pic.setRGB(16*previous.getX() + i, 16*previous.getY() + j, 255,
+       * 0, 0); } }
+       */
+      path += pathHelper(current, previous);
+      previous = current;
+      current = current.getParent();
+    }
+    // pic.saveImage();
+    return finish.getCostSoFar() + "\n"
+        + (new StringBuffer(path).reverse().toString());
+  }
+
+  /**
+   * A helper for constructPath that compares the x and y values of two nodes to
+   * determine whether a move of left, right, up, or down was made.
+   * 
+   * @return A single character string of L, R, D, or U.
+   * @see constructPath
+   */
+  public String pathHelper(Coordinate a, Coordinate b)
+  {
+    if ((a.getX() - b.getX()) == 1)
+    {
+      return "L";
+    }
+    if ((b.getX() - a.getX()) == 1)
+    {
+      return "R";
+    }
+    if ((a.getY() - b.getY()) == 1)
+    {
+      return "U";
+    }
+    if ((b.getY() - a.getY()) == 1)
+    {
+      return "D";
+    }
+    return "X";
+  }
+
 } // end class AStar
